51867daeea58ac63d0f6dde4cc2988de58d2456f
Taylor Huston <taylor.a.huston@gmail.com>
2016-03-19 23:58:19 -0700

    Add test for static route

--- server/tests/tests.js
+++ server/tests/tests.js
@@ -1,2 +1,17 @@
 var chai = require('chai');
+var chaiHttp = require('chai-http');
 var expect = chai.expect;
+var should = chai.should();
+var server = require('../../server');
+chai.use(chaiHttp);
+
+describe('staticRoute', function () {
+  it('should go to the home page', function (done) {
+    chai.request(server)
+      .get('/')
+      .end(function (err, res) {
+        res.should.have.status(200);
+        done();
+      });
+  });
+})
