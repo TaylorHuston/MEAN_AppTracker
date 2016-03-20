10301b1bf23c369e72e01df20390caae56cb7153
Taylor Huston <taylor.a.huston@gmail.com>
2016-03-19 23:31:32 -0700

    Move to config.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var config = require('./server/config/config.js')
 
 
 mongoose.connection.on('open', function (ref) {
   console.log('Connected to Mongo server...');
 });
 
-mongoose.connect('mongodb://taylor:testpassword@ds015869.mlab.com:15869/apptracker');
+mongoose.connect(config.database);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -15,6 +16,6 @@ app.get('/', function(req, res) {
   res.send("hello world");
 })
 
-app.listen(8080, function () {
+app.listen(config.port, function () {
   console.log('Example app listening on port 8080.');
 });
---
+++ server/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'port' : process.env.PORT || 8080,
+  'database' : 'mongodb://taylor:testpassword@ds015869.mlab.com:15869/apptracker'
+}
