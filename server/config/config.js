1aed279015a6cc6ed9e9345114bbc50a33d68cea
Taylor Huston <taylor.a.huston@gmail.com>
2016-03-28 01:21:00 -0700

    Fix secrets path

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,7 @@
-var SECRETS = require('secrets.js');
+var SECRETS = require('./secrets');
+
 
 module.exports = {
   'port' : process.env.PORT || 8080,
-  'database' : SECRETS.database;
+  'database' : SECRETS.database
 }
