45ab6137bf19f3f3cb899ef61f93436b4d6510e8
Taylor Huston <taylor.a.huston@gmail.com>
2016-03-31 00:54:34 -0700

    Set up some basic admin routes

--- server.js
+++ server.js
@@ -18,12 +18,17 @@ app.get('/', function(req, res) {
 })
 
 
-//Route for the admin section
+//Routes for the admin section
 var adminRouter = express.Router();
+
+adminRouter.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next();
+});
 
 //Route for the admin dashboard
 adminRouter.get('/', function(req, res) {
-  res.send("Dashboard");
+  res.send("Admin");
 });
 
 //Route for the Uuser page
@@ -31,7 +36,15 @@ adminRouter.get('/users', function(req, res) {
   res.send("Users");
 });
 
+//Routes for the user dashboard
+var dashboardRouter = express.Router();
+
+dashboardRouter.get('/', function(req, res) {
+  res.send("Dashboard");
+});
+
 app.use('/admin', adminRouter);
+app.use('/dashboard', dashboardRouter);
 
 //Start the server
 app.listen(config.port, function () {
